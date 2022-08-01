5e1e2000af4de270963cd7d848ab7eae3f321f8b
Kota <maedakoutaa@gmail.com>
2022-08-01 09:53:13 +0900

    Initial Commit

---
+++ MiniApp71-RxSwift13-TableView02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp71-RxSwift13-TableView02
+//
+//  Created by 前田航汰 on 2022/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
